95d06acefb9c430abd92a58e65c2b1ff0f199d29
Damian Peralta <peralta.damian@yahoo.com.mx>
2016-11-17 19:34:01 -0600

    Style todo app

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -35,7 +35,7 @@ var TodoApp = React.createClass({
         var updatedTodos = this.state.todos.map((todo) => {
             if (todo.id === id) {
                 todo.completed = !todo.completed;
-                todo.completedAt = todo.completed ? moment().unix() : undefined;                
+                todo.completedAt = todo.completed ? moment().unix() : undefined;
             }
             return todo;
         });
@@ -51,9 +51,17 @@ var TodoApp = React.createClass({
 
         return (
             <div>
-                <TodoSearch onSearch={this.handleSearch}/>
-                <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
-                <AddTodo onAddTodo={this.handleAddTodo}/>
+              <h1 className="page-title">Todo App</h1>
+              <div className="row">
+                <div className="column small-centered small-11 medium-6 large-5">
+                  <div className="container">
+                    <TodoSearch onSearch={this.handleSearch}/>
+                    <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
+                    <AddTodo onAddTodo={this.handleAddTodo}/>
+                  </div>
+                </div>
+              </div>
+
             </div>
 
         );
